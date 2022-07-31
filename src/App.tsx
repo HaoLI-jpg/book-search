0ff5b0b88a2c7c990dcea2af8ffdbaaf293d5ac6
Hao Li <dev64hao@gmail.com>
2022-07-31 19:46:10 +1200

    formatting the code

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
-import React, {useState, useEffect} from 'react';
-import { CircularProgress, Stack, Container, Grid, Typography, TextField, Button } from '@mui/material';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import {
+  CircularProgress,
+  Stack,
+  Container,
+  Grid,
+  Typography,
+  TextField,
+  Button,
+} from "@mui/material";
+import "./App.css";
 
 function App() {
   const [data, setData] = useState<any>("");
@@ -8,24 +16,25 @@ function App() {
 
   useEffect(() => {
     fetch("http://openlibrary.org/search.json?author=tolkien")
-      .then((response)=> response.json())
-      .then((bookData) => {setData(bookData); console.log(data)})
-    console.log(data);
+      .then((response) => response.json())
+      .then((bookData) => {
+        setData(bookData);
+      });
   }, []);
 
-  return(
+  return (
     <Container>
       <TextField
-      sx={{ m: 2 }}
-      value={search}
-      label="Search Authors Here..."
-      variant="filled"
-      size="medium"
-      onChange={(e) =>setSearch(e.target.value)}
+        sx={{ m: 2 }}
+        value={search}
+        label="Search Authors Here..."
+        variant="filled"
+        size="medium"
+        onChange={(e) => setSearch(e.target.value)}
       />
       <Button
         onClick={() => {
-          searchAuthor()
+          searchAuthor();
         }}
         sx={{ mt: 3 }}
       >
@@ -38,34 +47,44 @@ function App() {
           </Stack>
         </>
       ) : (
-          <Container>
-            <Typography gutterBottom variant="h2">List of Works</Typography>
-            {data.docs.map((e : any) => {
-              return(
-                      <Grid container columnSpacing="30px">
-                        <Grid item><a href={` https://openlibrary.org${e.key}/${e.title}`} target="_blank">{e.title}</a></Grid>
-                        <Grid item>{e.first_publish_year}</Grid>
-                      </Grid>
-                    )
-            })}
-          </Container>
+        <Container>
+          <Typography gutterBottom variant="h2">
+            List of Works
+          </Typography>
+          {data.docs.map((e: any) => {
+            return (
+              <Grid container columnSpacing="30px">
+                <Grid item>
+                  <a
+                    href={` https://openlibrary.org${e.key}/${e.title}`}
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    {e.title}
+                  </a>
+                </Grid>
+                <Grid item>{e.first_publish_year}</Grid>
+              </Grid>
+            );
+          })}
+        </Container>
       )}
     </Container>
-  )
+  );
 
   function searchAuthor() {
     console.log(search);
 
-    if(searchAuthor === undefined){
+    if (searchAuthor === undefined) {
       return;
     }
 
     fetch("http://openlibrary.org/search.json?author=" + search)
-      .then((response)=> response.json())
-      .then((bookData) => {setData(bookData); console.log(data); console.log(search)})
+      .then((response) => response.json())
+      .then((bookData) => {
+        setData(bookData);
+      });
   }
 }
-
-
 
 export default App;
