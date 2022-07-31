6c9638926da18008e18ff888dbee82c71eb91021
Hao Li <dev64hao@gmail.com>
2022-07-31 19:24:41 +1200

    finished msa frontend assignment

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
+import { CircularProgress, Stack, Container, Grid, Typography, TextField, Button } from '@mui/material';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [data, setData] = useState<any>("");
+  const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    fetch("http://openlibrary.org/search.json?author=tolkien")
+      .then((response)=> response.json())
+      .then((bookData) => {setData(bookData); console.log(data)})
+    console.log(data);
+  }, []);
+
+  return(
+    <Container>
+      <TextField
+      sx={{ m: 2 }}
+      value={search}
+      label="Search Here..."
+      variant="filled"
+      size="medium"
+      onChange={(e) =>setSearch(e.target.value)}
+      />
+      <Button
+        onClick={() => {
+          searchAuthor()
+        }}
+        sx={{ mt: 3 }}
+      >
+        Search
+      </Button>
+      {!data ? (
+        <>
+          <Stack alignItems="center" justifyContent="center" margin="300px">
+            <CircularProgress />
+          </Stack>
+        </>
+      ) : (
+          <Container>
+            <Typography gutterBottom variant="h2">List of Works</Typography>
+            {data.docs.map((e : any) => {
+              return(
+                      <Grid container columnSpacing="30px">
+                        <Grid item><a href={` https://openlibrary.org${e.key}/${e.title}`} target="_blank">{e.title}</a></Grid>
+                        <Grid item>{e.first_publish_year}</Grid>
+                      </Grid>
+                    )
+            })}
+          </Container>
+      )}
+    </Container>
+  )
+
+  function searchAuthor() {
+    console.log(search);
+
+    if(searchAuthor === undefined){
+      return;
+    }
+
+    fetch("http://openlibrary.org/search.json?author=" + search)
+      .then((response)=> response.json())
+      .then((bookData) => {setData(bookData); console.log(data); console.log(search)})
+  }
 }
+
+
 
 export default App;
